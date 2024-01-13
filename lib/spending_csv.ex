1aac31390966229ba3588c192e368df7e9be22e5
June Kelly <june@cnjctr.com>
2024-01-13 14:58:24 +0000

    Working

--- lib/spending_csv.ex
+++ lib/spending_csv.ex
@@ -5,10 +5,11 @@ defmodule SpendingCsv do
 
   require Logger
 
-  def run() do
-    [input_path] = System.argv()
-
-    Logger.info("Loading from #{input_path}")
+  def main(args) do
+    {_flags, [input_path], _errors} = OptionParser.parse(args, strict: [])
+    parsed = load_from_file(input_path)
+    processed = process(parsed)
+    print_csv(processed)
   end
 
   def load_from_file(path) do
@@ -16,8 +17,28 @@ defmodule SpendingCsv do
     |> YamlElixir.read_from_file!()
   end
 
+  def process(parsed) do
+    Enum.map(parsed, fn date_map ->
+      Enum.map(date_map, fn {date, entries} ->
+        Enum.map(entries, fn entry ->
+          {price, description} = parse_entry(entry)
+          {date, price, description}
+        end)
+      end)
+    end)
+    |> List.flatten()
+  end
+
   def parse_entry(entry) do
     [^entry, price, description] = Regex.run(~r/([^ ]+) (.*)/, entry)
     {price, description}
+  end
+
+  def print_csv(processed) do
+    IO.puts("date;price;description")
+
+    Enum.each(processed, fn {date, price, description} ->
+      Enum.join([date, price, description], ";") |> IO.puts()
+    end)
   end
 end
